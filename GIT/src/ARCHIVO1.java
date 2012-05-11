c0cf8768d32226a45f65aaf68cfc5f8b4571b3b9
dsanchez <dsanchez@106-16.info2>
2012-05-11 10:05:44 +0200

    cambiar el hola mamon por hola tierra

--- GIT/src/ARCHIVO1.java
+++ GIT/src/ARCHIVO1.java
@@ -4,6 +4,6 @@ public class ARCHIVO1 {
 	public static void main(String[] args) {
 	   
 		System.out.println("Hola mundo");
-		System.out.println("hola mamon");
+		System.out.println("hola tierra");
 	  }
 }
